641edc06fe7ca8f29102453bd1e157a1c86df326
Devin Medeiros <devin_medeiros@Devin-Medeiross-MacBook-Pro.local>
2014-04-04 13:13:03 -1000

    Initial Commit

---
+++ Git Test/CKViewController.h
@@ -0,0 +1,13 @@
+//
+//  CKViewController.h
+//  Git Test
+//
+//  Created by Devin Medeiros on 4/4/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Devin Medeiros on 4/4/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CKAppDelegate class]));
+    }
+}
