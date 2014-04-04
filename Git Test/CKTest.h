b092989ee9e34a85c379bb153a2438165cd9f2f0
Devin Medeiros <devin_medeiros@Devin-Medeiross-MacBook-Pro.local>
2014-04-04 13:22:22 -1000

    added a test class

---
+++ Git Test/CKTest.h
@@ -0,0 +1,13 @@
+//
+//  CKTest.h
+//  Git Test
+//
+//  Created by Devin Medeiros on 4/4/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKTest : NSObject
+
+@end
---
+++ Git Test/CKTest.m
@@ -0,0 +1,13 @@
+//
+//  CKTest.m
+//  Git Test
+//
+//  Created by Devin Medeiros on 4/4/14.
+//  Copyright (c) 2014 Cutie Killer Apps. All rights reserved.
+//
+
+#import "CKTest.h"
+
+@implementation CKTest
+
+@end
